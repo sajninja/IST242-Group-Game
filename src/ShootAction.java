1685cd90cca3d2576ce5e86bb79903718a351519
saj <92606915+sajninja@users.noreply.github.com>
2025-06-25 14:36:21 -0400

    Update ShootAction.java

--- src/ShootAction.java
+++ src/ShootAction.java
@@ -5,24 +5,40 @@
  * Move actions extend Action and contain a projectile and shoot type.
  */
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class ShootAction extends Action {
 
     private Projectile projectile;
     private ShootType shootType;
 
-    public ShootAction(Projectile projectile, ShootType shootType) {
+    public ShootAction(Projectile projectile, ShootType shootType, int weight) {
         this.projectile = projectile;
         this.shootType = shootType;
+        setWeight(weight);
     }
 
 
     @Override
     public boolean checkCondition() {
-        return false;
+        return true;
     }
 
     @Override
-    public boolean doAction() {
-        return false;
+    public void doAction() {
+
+    }
+
+    public ArrayList<Projectile> shootProjectile(Vector2 position) {
+        ArrayList<Projectile> output = new ArrayList<>();
+        Projectile projectile = new Projectile(this.projectile);
+        projectile.setPosition(new Vector2(position.getX(), position.getY()));
+        Vector2 velocity = Vector2.AtoB(new Vector2(Game.player.getTruePosition().getX(), Game.player.getTruePosition().getY()), position);
+        velocity.normalize(projectile.speed);
+        projectile.setVelocity(velocity);
+
+        output.add(projectile);
+        return output;
     }
 }
